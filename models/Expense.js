b77ced825fb3f9fa13f8c89b0d1edffed163ea49
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-07-17 10:13:34 +0100

    update models

--- models/Expense.js
+++ models/Expense.js
@@ -5,8 +5,8 @@ const expenseSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   category: { type: String, required: true },
   description: { type: String },
-  amount: { type: Number },
-  date: { type: Date, default: new Date().toISOString() }
+  amount: { type: Number, required: true },
+  date: { type: Date, required: true }
 
 }, { timestamps: true });
 
--- models/User.js
+++ models/User.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const budgetModel = require('./Budget');
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, unique: true },
   expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Expense" }],
   budget: { type: mongoose.Schema.Types.ObjectId, ref: "Budget" }
 
 }, { timestamps: true });
 
-// returns user without password
+userSchema.pre('save', async function () {
+  if (!this.isNew) return;
+
+  const budget = await budgetModel.create({ user: this._id });
+  this.budget = budget._id;
+});
 
 module.exports = mongoose.model('User', userSchema);
